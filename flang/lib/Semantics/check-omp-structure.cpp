5fb434e8f5a0d11d13d2b20ae5188a1f4f592e54
Peter Klausler <pklausler@nvidia.com>
2022-04-05 13:42:12 -0700

    [flang] Allow modification of construct entities
    Construct entities from ASSOCIATE, SELECT TYPE, and SELECT RANK
    are modifiable if the are associated with modifiable variables
    without vector subscripts.  Update WhyNotModifiable() to accept
    construct entities that are appropriate.
    
    A need for more general error reporting from one overload of
    WhyNotModifiable() caused its result type to change to
    std::optional<parser::Message> instead of ::MessageFixedText,
    and this change had some consequences that rippled through
    call sites.
    
    Some test results that didn't allow for modifiable construct
    entities needed to be updated.
    
    Differential Revision: https://reviews.llvm.org/D123722

--- flang/lib/Semantics/check-omp-structure.cpp
+++ flang/lib/Semantics/check-omp-structure.cpp
@@ -1575,10 +1575,12 @@ void OmpStructureChecker::CheckIntentInPointerAndDefinable(
         }
         if (auto msg{
                 WhyNotModifiable(*symbol, context_.FindScope(name->source))}) {
-          context_.Say(GetContext().clauseSource,
-              "Variable '%s' on the %s clause is not definable"_err_en_US,
-              symbol->name(),
-              parser::ToUpperCaseLetters(getClauseName(clause).str()));
+          context_
+              .Say(GetContext().clauseSource,
+                  "Variable '%s' on the %s clause is not definable"_err_en_US,
+                  symbol->name(),
+                  parser::ToUpperCaseLetters(getClauseName(clause).str()))
+              .Attach(std::move(*msg));
         }
       }
     }
@@ -2147,7 +2149,7 @@ void OmpStructureChecker::CheckDefinableObjects(
               "Variable '%s' on the %s clause is not definable"_err_en_US,
               symbol->name(),
               parser::ToUpperCaseLetters(getClauseName(clause).str()))
-          .Attach(source, std::move(*msg), symbol->name());
+          .Attach(std::move(*msg));
     }
   }
 }
