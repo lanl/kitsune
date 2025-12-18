stelleg/llvm-project

GPU reductions via stripmining pass working

clang/lib/CodeGen/CGKitsune.cpp
@@ -111,6 +111,8 @@ CodeGenFunction::GetTapirTargetAttr(ArrayRef<const Attr *> Attrs) {
         return llvm::TapirTargetID::Cuda;
       case TapirTargetAttr::Hip:
         return llvm::TapirTargetID::Hip;
+      case TapirTargetAttr::GPU:
+        return llvm::TapirTargetID::GPU; 
       case TapirTargetAttr::OpenCilk:
         return llvm::TapirTargetID::OpenCilk;
       case TapirTargetAttr::OpenMP:

lanl/kitsune

clang/lib/CodeGen/CGKitsune.cpp
@@ -106,6 +106,8 @@ CodeGenFunction::GetTapirTargetAttr(ArrayRef<const Attr *> Attrs) {
         return llvm::TTID::Cuda;
       case TapirTargetAttr::Hip:
         return llvm::TTID::Hip;
+      case TapirTargetAttr::GPU:
+        return llvm::TTID::GPU; 
       case TapirTargetAttr::OpenCilk:
         return llvm::TTID::OpenCilk;
       case TapirTargetAttr::OpenMP: