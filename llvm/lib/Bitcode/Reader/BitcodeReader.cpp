stelleg/llvm-project

working reductions on 16.x

llvm/lib/Bitcode/Reader/BitcodeReader.cpp
@@ -2084,6 +2084,8 @@ static Attribute::AttrKind getAttrFromCode(uint64_t Code) {
     return Attribute::ReducerRegister;
   case bitc::ATTR_KIND_REDUCER_UNREGISTER:
     return Attribute::ReducerUnregister;
+  case bitc::ATTR_KIND_KITSUNE_REDUCTION:
+    return Attribute::KitsuneReduction;
   }
 }
 

llvm/lib/Bitcode/Writer/BitcodeWriter.cpp
@@ -825,6 +825,8 @@ static uint64_t getAttrKindEncoding(Attribute::AttrKind Kind) {
     return bitc::ATTR_KIND_REDUCER_UNREGISTER;
   case Attribute::ReducerRegister:
     return bitc::ATTR_KIND_REDUCER_REGISTER;
+  case Attribute::KitsuneReduction:
+    return bitc::ATTR_KIND_KITSUNE_REDUCTION; 
   case Attribute::EndAttrKinds:
     llvm_unreachable("Can not encode end-attribute kinds marker.");
   case Attribute::None:

lanl/kitsune

llvm/lib/Bitcode/Reader/BitcodeReader.cpp
@@ -2260,6 +2260,8 @@ static Attribute::AttrKind getAttrFromCode(uint64_t Code) {
     return Attribute::KitKernel;
   case bitc::ATTR_KIND_KIT_DEVICE:
     return Attribute::KitDevice;
+  case bitc::ATTR_KIND_KIT_REDUCTION:
+    return Attribute::KitsuneReduction;
   }
 }
 

llvm/lib/Bitcode/Writer/BitcodeWriter.cpp
@@ -960,6 +960,8 @@ static uint64_t getAttrKindEncoding(Attribute::AttrKind Kind) {
     return bitc::ATTR_KIND_KIT_KERNEL;
   case Attribute::KitDevice:
     return bitc::ATTR_KIND_KIT_DEVICE;
+  case Attribute::KitsuneReduction:
+    return bitc::ATTR_KIND_KIT_REDUCTION; 
   case Attribute::EndAttrKinds:
     llvm_unreachable("Can not encode end-attribute kinds marker.");
   case Attribute::None: